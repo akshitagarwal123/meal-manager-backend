b17890fb458b0a43747c5e2a5437e171091f790f
akshitagarwal123 <akshitagarwal10505@gmail.com>
2025-09-23 02:24:06 +0530

    updated secure connection to db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
 
+console.log('DB config:', {
+  user: process.env.PGUSER || 'postgres',
+  host: process.env.PGHOST || 'localhost',
+  database: process.env.PGDATABASE || 'meal_manager',
+  port: process.env.PGPORT || 5432
+});
+
 const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'meal_manager',
   password: process.env.PGPASSWORD || 'password',
   port: process.env.PGPORT || 5432,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
