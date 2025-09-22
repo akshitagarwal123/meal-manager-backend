0ce93eed751401e7cb2f7ca42d52aaf8c2736d19
akshitagarwal123 <akshitagarwal10505@gmail.com>
2025-09-23 02:13:14 +0530

    SSl/TLS logic enable while sending otp

--- config/email.js
+++ config/email.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
